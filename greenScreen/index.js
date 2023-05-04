720a583e80d0e6adc7a6b4fbee7d84cba0bfb50c
APatel-AI <arpan.p.se@gmail.com>
2023-05-04 09:14:37 -0400

    Green Screen Launch

---
+++ greenScreen/index.js
@@ -0,0 +1,7 @@
+const changeColor = document.getElementById("changeColor");
+const colorInput = document.getElementById("color");
+
+changeColor.addEventListener("click", function() {
+  const color = colorInput.value;
+  document.body.style.backgroundColor = color;
+});
